fc3730c7f936cab579c4c6b597e51b9454acfe05
aeowa <aeowa@mail.ru>
2015-11-06 10:48:17 +0300

    index.html

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
     autoPlay: 7000, rewindSpeed: 5000, slideSpeed: 3000,});
 
 
+  jQuery('#camera-slideshow').camera({
+    pagination: false,
+    thumbnails: true,
+    navigation: false,
+    playPause: false,
+    imagePath: 'img/',
+    thumbUrl: 'img/thumbs',
+    overlayer: true,
+    barPosition: 'right',
+    hover: false,
+    loader: false
+  });
 });
 
 
